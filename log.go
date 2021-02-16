21370a170de3ef88d413c57f96d1295e48fb5ea8
pjones <pjones@ellem.ai>
2021-02-15 22:03:10 -0600

    comment additions

--- log.go
+++ log.go
@@ -2,81 +2,104 @@ package main
 
 import (
 	"fmt"
-
-	"./lmgo/log"
 )
 
-var Reset  = "\033[0m"
-var Red    = "\033[31m"
-var Green  = "\033[32m"
-var Yellow = "\033[33m"
-var Blue   = "\033[34m"
-var Purple = "\033[35m"
-var Cyan   = "\033[36m"
-var Gray   = "\033[37m"
-var White  = "\033[97m"
+// Logging Color Constants
+const (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
+	Gray   = "\033[37m"
+	White  = "\033[97m"
+)
 
+// Logging Level Constants
+const (
+	LevelEmerg uint = iota
+	LevelAlert
+	LevelCrit
+	LevelError
+	LevelWarning
+	LevelNotice
+	LevelInfo
+	LevelDebug
+)
+
+// Our Logging struct which implements allmanf
 type Logger struct{}
 
+
+/******* Allmanf Method Implementations *******/
+
 func (Logger) Emergf(format string, args ...interface{}) {
-	lmlog(log.LevelEmerg,format, args...)
+	lmlog(LevelEmerg,format, args...)
 }
 
 func (Logger) Critf(format string, args ...interface{}) {
-	lmlog(log.LevelCrit,format, args...)
+	lmlog(LevelCrit,format, args...)
 }
 
 func (Logger) Errorf(format string, args ...interface{}) {
-	lmlog(log.LevelError,format, args...)
+	lmlog(LevelError,format, args...)
 }
 
 func (Logger) Alertf(format string, args ...interface{}) {
-	lmlog(log.LevelAlert,format, args...)
+	lmlog(LevelAlert,format, args...)
 }
 
 func (Logger) Warnf(format string, args ...interface{}) {
-	lmlog(log.LevelWarning,format, args...)
+	lmlog(LevelWarning,format, args...)
 }
 
 func (Logger) Noticef(format string, args ...interface{}) {
-	lmlog(log.LevelNotice,format, args...)
+	lmlog(LevelNotice,format, args...)
 }
 
 func (Logger) Infof(format string, args ...interface{}) {
-	lmlog(log.LevelInfo, format, args...)
+	lmlog(LevelInfo, format, args...)
 }
 
 func (Logger) Debugf(format string, args ...interface{}) {
-	lmlog(log.LevelDebug, format, args...)
+	lmlog(LevelDebug, format, args...)
 }
 
+
+/******* Helper Methods *******/
+
+// lmlog prints a log to the console using fmt.Printf
+// It formats the output with a predefined color based on
+// level provided
 func lmlog(level uint, format string, args ...interface{}) {
 
 	prefixColor := ""
 
 	switch level {
-	case log.LevelDebug:
+	case LevelDebug:
 		prefixColor = ""
 		break
-	case log.LevelInfo:
+	case LevelInfo:
 		prefixColor = White
 		break
-	case log.LevelNotice:
+	case LevelNotice:
 		prefixColor = White
 		break
-	case log.LevelWarning:
+	case LevelWarning:
 		prefixColor = Yellow
 		break
-	case log.LevelAlert:
+	case LevelAlert:
 		prefixColor = Yellow
 		break
-	case log.LevelError:
+	case LevelError:
 		prefixColor = Red
 		break
-	case log.LevelCrit:
+	case LevelCrit:
 		prefixColor = Red
 		break
-	case log.LevelEmerg:
+	case LevelEmerg:
 		prefixColor = Red
 		break
 	default:
